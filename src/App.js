8b04bb1d56eafa2214e54561e5df78a8c1cdbe57
chisomoguh <oguhc@carleton.edu>
2022-11-08 13:46:01 -0600

    adding pages, routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
+// import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+// import Home from './pages';
+// import About Us from './pages/about';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import SankeyChart from './components/SankeyChart'
-function App() {
-  return (
-    <div className="App">
-        <div class="h1">
-            <p>MoneyFlows</p>
-        </div>
-      <SankeyChart />
-    </div>
 
-  )
+
+import HomePage from './pages/HomePage/index.js'
+// import AboutUs from './pages/AboutPage/index.js'
+
+function App() {
+return (
+  <>
+      <p> Hello </p>
+      <HomePage />
+      {/* <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/'>
+            <HomePage />
+          </Route>
+          <Route path='/about'>
+            <AboutUs />
+          </Route>
+          <Route path='/contact' element={<Contact/>} />
+          <Route path='/blogs' element={<Blogs/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
+        </Routes>
+      </Router> */}
+  </>
+);
 }
-export default App
+
+export default App;
