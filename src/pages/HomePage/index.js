c6899ec0b6d9f875931750c86ab5ed7229c94f0d
root <root@H6PS7S2.localdomain>
2022-11-08 13:01:06 -0600

    added infrastructure for papes of site

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,14 @@
+
+
+function HomePage() {
+    return (
+        <>
+            <div className="App">
+                <div class="h1">
+                    <p>MoneyFlows</p>
+                </div>
+                <SankeyChart />
+            </div>
+        </>
+    );
+}
