8601d2f8e61b53fb7cf04b6151ba099010e0e9e6
chisomoguh <oguhc@carleton.edu>
2022-11-08 13:46:01 -0600

    adding pages, routing

---
+++ src/pages/AboutPage/index.js
@@ -0,0 +1,9 @@
+
+
+function AboutUs() {
+    return (
+        <p>Hello</p>
+    );
+}
+
+export default AboutUs;
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,14 +1,20 @@
-
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import SankeyChart from './components/SankeyChart'
+import AboutUs from "../AboutPage/index.js"
 
 function HomePage() {
     return (
         <>
-            <div className="App">
+            <div className="HomePage">
                 <div class="h1">
                     <p>MoneyFlows</p>
                 </div>
                 <SankeyChart />
             </div>
+
+            <button onClick={<AboutUs/>}>About Us</button>
         </>
     );
 }
+
+export default HomePage;
