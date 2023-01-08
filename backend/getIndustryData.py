003104b3cad099e26f4c69b8ce4f6eb19f034fb2
justlitatheng <56949321+justlitatheng@users.noreply.github.com>
2023-01-08 12:37:40 -0600

    Revert "get backend politicians and industry server set up"
    This reverts commit b6562da43661fbc49d4f506587c8d3289e2b64bd.
    
    revert to working format.

--- backend/getIndustryData.py
+++ backend/getIndustryData.py
@@ -4,8 +4,7 @@ import xmltodict
 from collections import defaultdict
 import requests
 
-path_to_industries = '../interim_data/CRPIndustryCodes.csv'
-path_to_legislators = '../interim_data/legislators_current.csv'
+path_to_legislators = 'interim_data\legislators_current.csv'
 # path_to_industries = '/Users/kevin/Downloads/CRPIndustryCodes.csv'
 key = "91a96cc61cceb54c2473df69372795f6" # API key
 
@@ -21,7 +20,7 @@ def total_from_industry_by_cid(ind, cid):
             print(
                 f"There's a {response.status_code} error with your request")
 
-# saves a list of dictionaries containing demographic information on congresspeople to our database 
+# returns a list of dictionaries containing all information on congresspeople
 def get_politician_data():
     with open(path_to_legislators, 'r') as csvfile:
         csv_reader = csv.reader(csvfile)
@@ -35,13 +34,7 @@ def get_politician_data():
             for index, var in enumerate(column_names):
                 new_dict[var] = row[index]
             list_of_dicts.append(new_dict)
-
-        # insert into MongoDB database
-        client = pymongo.MongoClient("mongodb://localhost:27017/")
-        db = client['comps']
-        new_collection = db["congresspeople"] # create a new collection in the database
-        new_collection.drop() # clear anything already in it
-        new_collection.insert_many(list_of_dicts)
+        return list_of_dicts
 
 # returns a list of all candidate cid's
 def get_all_cid():
@@ -56,64 +49,54 @@ def get_all_cid():
 # returns a list of all industry codes and names in the following format:
 # industry code [blank space] industry name
 # A01 Banking
-def get_all_industries():
-    with open(path_to_industries, 'r') as csvfile:
-        csv_reader = csv.reader(csvfile)
-        industries_set = set()
-        temp = next(csv_reader)
-        for row in csv_reader:
-            industries_set.add(row[2] + " " + row[3])
-        return list(industries_set)
+# def get_all_industries():
+#     with open(path_to_industries, 'r') as csvfile:
+#         csv_reader = csv.reader(csvfile)
+#         industries_set = set()
+#         temp = next(csv_reader)
+#         for row in csv_reader:
+#             industries_set.add(row[2] + " " + row[3])
+#         return list(industries_set)
 
-# saves industry donation data to congresspeople (grouped by industry) to our database
-# takes ~ 4 days to completely run.
-# DO NOT RUN UNLESS YOU KNOW WHAT YOU'RE DOING. It will clear everything in our database and will take days to rerun it.
-def get_industry_data():
-    industry_dict = defaultdict(list)
-    cid_list = get_all_cid()
-    industry_list = get_all_industries()
+# def get_industry_totals():
+#     industry_dict = defaultdict(list)
+#     cid_list = get_all_cid()
+#     industry_list = get_all_industries()
 
-    cid_list = cid_list[:10]
-    industry_list = industry_list[:10]
-    # insert into MongoDB database
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client['comps']
-    new_collection = db["industries"] # create a new collection in the database
-    new_collection.drop() # clear anything already in it
+    # these are just for demonstrating on a smaller dataset
+    # cid_list = cid_list[:5]
+    # industry_list = industry_list[:5]
 
-    count = 0 # counts number of calls we have made in order to make sure we don't exceed 20000 calls per day
-    for industry in industry_list:
-        total = 0 # keeps track of total amount of donations from this industry
-        for cid in cid_list:
-            response = total_from_industry_by_cid(industry[:3], cid)
-            count += 1
-            print(count)
-            if count > 15000:
-                count = 0
-                time.sleep(90000)
-            if response:
-                data_dict = xmltodict.parse(response.text) # parse from XML to a JSON-dict format
-                new_cand_dict = {}
-                new_cand_dict["cid"] = data_dict["response"]["candIndus"]["@cid"]
-                new_cand_dict["name"] = data_dict["response"]["candIndus"]["@cand_name"]
-                donation_amount = int(data_dict["response"]["candIndus"]["@total"])
-                new_cand_dict["total"] = donation_amount
-                total += donation_amount # updates total amount of donations
-                industry_dict[industry].append(new_cand_dict) # adds this candidate's info to the list 
+#     for industry in industry_list:
+#         for cid in cid_list:
+#             response = total_from_industry_by_cid(industry[:3], cid)
+#             if response:
+#                 data_dict = xmltodict.parse(response.text) # parse from XML to a JSON-dict format
+#                 new_cand_dict = {}
+#                 new_cand_dict["cid"] = data_dict["response"]["candIndus"]["@cid"]
+#                 new_cand_dict["name"] = data_dict["response"]["candIndus"]["@cand_name"]
+#                 new_cand_dict["total"] = int(data_dict["response"]["candIndus"]["@total"])
+#                 industry_dict[industry].append(new_cand_dict)
 
-        new_dict = {"code": industry[:3], "name" : industry[4:], "total": total, "congresspeople" : industry_dict[industry]}
-        new_collection.insert_one(new_dict)
+#     # insert into MongoDB database
+#     client = pymongo.MongoClient("mongodb://localhost:27017/")
+#     db = client['comps']
+#     new_collection = db["industries"] # create a new collection in the database
+#     new_collection.drop() # clear anything already in it
+
+#     for industry, congresspeople in industry_dict.items():
+#         industry_code = industry[:3]
+#         industry_name = industry[4:]
+
+#         new_dict = {"code": industry_code, "name" : industry_name, "congresspeople" : congresspeople}
+#         new_collection.insert_one(new_dict)
+
+# get_industry_totals()
+
+print(get_all_cid())
+
+
+
+        
     
-# Returns a list of industries with available data in the following form: "code industry_name". Ex "W04 Education"
-def getNonEmptyIndustries():
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client['comps']
-    new_collection = db["industries"] # create a new collection in the database
-    industries = []
-    for industry in new_collection.find():
-        industry_code = industry['code']
-        industry_name = industry['name']
-        industries.append(industry_code + " " + industry_name)
-    return industries
 
-get_industry_data()
