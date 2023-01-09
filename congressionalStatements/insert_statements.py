1f72451c069b29d8dd8a3c922ef036ed7571ca9f
Anna Neiman-Golden <annaneiman-golden@Annas-MacBook-Air-3.local>
2023-01-09 16:53:28 -0600

    cleaning and documentation

--- congressionalStatements/insert_statements.py
+++ congressionalStatements/insert_statements.py
@@ -1,46 +1,42 @@
 import datetime
 import sys
 import requests
-
 import pymongo
 import pandas as pd
-
 from parse_pdf import *
 
+# get dataframe of congressional record pdfs
 def get_cr_data():
     return pd.DataFrame.from_pickle("../CR_API/cr_117")
 
-def add_congress_record_id(collection):
+# add congressional record identifier as a field in congresspeople_collection
+def add_congress_record_id(congresspeople_collection):
     state_dict = {"AL":"Alabama","AK":"Alaska","AZ":"Arizona","AR":"Arkansas","CA":"California","CO":"Colorado","CT":"Connecticut","DE":"Delaware","FL":"Florida","GA":"Georgia","HI":"Hawaii","ID":"Idaho","IL":"Illinois","IN":"Indiana","IA":"Iowa","KS":"Kansas","KY":"Kentucky","LA":"Louisiana","ME":"Maine","MD":"Maryland","MA":"Massachusetts","MI":"Michigan","MN":"Minnesota","MS":"Mississippi","MO":"Missouri","MT":"Montana","NE":"Nebraska","NV":"Nevada","NH":"New Hampshire","NJ":"New Jersey","NM":"New Mexico","NY":"New York","NC":"North Carolina","ND":"North Dakota","OH":"Ohio","OK":"Oklahoma","OR":"Oregon","PA":"Pennsylvania","RI":"Rhode Island","SC":"South Carolina","SD":"South Dakota","TN":"Tennessee","TX":"Texas","UT":"Utah","VT":"Vermont","VA":"Virginia","WA":"Washington","WV":"West Virginia","WI":"Wisconsin","WY":"Wyoming"}
 
-    # need to fix this to include ppl with same name and same state
-    # test adding congress_record_id field
-
-    for doc in collection.find():
+    for doc in congresspeople_collection.find():
         try:
             last_name = doc['last_name']
             state_code = doc['state']
             state = state_dict[state_code]
-            # if same last name, chamber, state, and sex (?), include full name -- may need to double check this, could be an issue tho
-            if collection.count_documents({'last_name' : last_name, 'type' : doc['type'], 'gender' : doc['gender'], 'state' : state_code}) > 1 :
+            # check for ambiguous names, assign id
+            if congresspeople_collection.count_documents({'last_name' : last_name, 'type' : doc['type'], 'gender' : doc['gender'], 'state' : state_code}) > 1 : 
                 congress_record_id = doc['first_name'].upper() + " " + last_name.upper() + " of " + state
-            elif collection.count_documents({'last_name' : last_name, 'type' : doc['type']}) > 1: #if ambiguous last name
+            elif congresspeople_collection.count_documents({'last_name' : last_name, 'type' : doc['type']}) > 1: 
                 congress_record_id = last_name.upper() + " of " + state
             else:
                 congress_record_id = last_name.upper()
-            
-            collection.update_one(
+
+            congresspeople_collection.update_one(
                 {"_id" : doc["_id"]}, 
                 {"$set": {"congress_record_id": congress_record_id}})
         except KeyError:
             pass
 
+# store speeches in statemenets_collection by opensecrets_id
 def store_speeches(speech_list, statements_collection, congresspeople_collection, date):
-    # store in a mongodb collection
     date = str(date)
     for speech in speech_list:
         opensecrets_id = get_id_from_speech(speech, congresspeople_collection)
-        # speech = re.sub('(?:Mr|Ms|Mrs). ' + memberName, '', speech) # filter out names at beginning  
 
         # if document already exists, update
         if statements_collection.count_documents({"opensecrets_id": opensecrets_id, "date" : date}) > 0:
@@ -55,14 +51,14 @@ def store_speeches(speech_list, statements_collection, congresspeople_collection
             }
             statements_collection.insert_one(document)
 
+# test contents of a collection
 def check_collection(collection):
-    # test contents of collection -- good so far!!
     for doc in collection.find():
         print("NEW DOC")
         print(doc)
 
 def get_mongodb_client():
-    client = pymongo.MongoClient("mongodb://localhost:27017/") # must be run on olin312-04!!
+    client = pymongo.MongoClient("mongodb://localhost:27017/") # must be run on olin312-04
     db = client["comps"] # access the comps db
     return db
 
@@ -74,12 +70,13 @@ def get_congresspeople_collection(db):
     collection = db["congresspeople"]
     return collection
 
+# read in and store speeches from all pdfs
 def main(argv):
     db = get_mongodb_client()
     statements_collection = get_statements_collection(db)
     congresspeople_collection = get_congresspeople_collection(db)
     add_congress_record_id(congresspeople_collection)
-    #check_collection(congresspeople_collection)
+    # check_collection(congresspeople_collection)
 
     try: 
         df = pd.read_pickle(argv[0])
@@ -88,20 +85,20 @@ def main(argv):
     except IndexError as e:
         print(e)
 
-    #test_pdf = 'https://www.congress.gov/117/crec/2021/01/28/167/17/CREC-2021-01-28-senate.pdf'
-    #date = datetime.date(2021,1,28)
     num = 0 # progress counter
     for row in df.itertuples():
         if num % 10 == 0:
             print(num)
+
         with open("temp.pdf", "wb") as pdf:
             pdf.write(requests.get(row.pdf_url).content)
         pdf_text = load_pdf("temp.pdf")
         date = row.date.date()
         speeches = extract_speeches(pdf_text, date, row.chamber, congresspeople_collection)
         store_speeches(speeches, statements_collection, congresspeople_collection, date)
+
         num += 1
-    check_collection(statements_collection)
+    # check_collection(statements_collection)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- congressionalStatements/parse_pdf.py
+++ congressionalStatements/parse_pdf.py
@@ -1,119 +1,60 @@
 import re
 import datetime
-
 import PyPDF2
 import pymongo
 
-
+# get text of pdf as one long string
 def load_pdf(pdf_file):
     with open(pdf_file, 'rb') as f:
-    #pdfFile = open('/Users/annaneiman-golden/Downloads/CREC-2021-03-23-senate.pdf', 'rb')
         pdf_reader = PyPDF2.PdfFileReader(f)
-        # Read in pages, concat into one long string
         pdf_text = ''
         for i in range(pdf_reader.numPages):
             pdf_text += pdf_reader.getPage(i).extractText()
     return pdf_text
-
-# client = pymongo.MongoClient("mongodb+srv://neimana:earthsayshello@cluster0.pvfbgyu.mongodb.net/?retryWrites=true&w=majority")
-# db = client['statements_database']
-# statementCollection = db['statements']
-# statementCollection.drop()
-#congressCollection = db['test for now']
 
 #regex variables
 #memberName = '[A-Z][A-Z]+(?:[ -][A-Z][A-Z]+)?' # this is temp, will update with a function to get more like the one below # [A-Z][A-Z]+ -- old one
 date = '(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|June?|July?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?) [0-9]{1,2}, [0-9]{4}'
 states = 'Alabama|Alaska|Arizona|Arkansas|California|Colorado|Connecticut|Delaware|Florida|Georgia|Hawaii|Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|Michigan|Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New Hampshire|New Jersey|New Mexico|New York|North Carolina|North Dakota|Ohio|Oklahoma|Oregon|Pennsylvania|Rhode Island|South Carolina|South Dakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West Virginia|Wisconsin|Wyoming'
 
-# from congress collection, make a regex for all members of congress names -- or congress ID?
-def get_congress_record_id_regex(collection):
+# from congresspeople_collection, make a regex for congress_record_ids
+def get_congress_record_id_regex(congresspeople_collection):
     ids = []
-    for doc in collection.find():
+    for doc in congresspeople_collection.find():
         try:
             ids.append(doc['congress_record_id'])
         except KeyError:
-            # not from a state - will filter these members out
+            # filter out members in collection not from a state
             pass
     return '|'.join(ids) 
 
-# def getDateAndChamber(pdfName):
-#     '''
-#     get date and chamber from input (paired with links)
-#     '''
-#     date = re.search('[0-9]{4}-[0-9]{2}-[0-9]{2}', pdfName).group()
-#     # chamber = re.search('senate|house', pdfName).group() -- when using chamber specific docs
-#     chamber = ""
-#     return date, chamber
-
-# helper methods to get opensecrets id from speech
-def get_id_from_speech(speech, congressperson_collection):
-    member_ids = get_congress_record_id_regex(congressperson_collection)
-    #congressperson_id = re.search('(?:Mr|Ms|Mrs). (' + member_ids + ')', speech).group(0) #changed here to group -- works?
-    congressperson_id = re.search(member_ids, speech).group(0) # if not, try this
-
+# identify author of speech and return corresponding opensecrets_id
+def get_id_from_speech(speech, congresspeople_collection):
+    member_ids = get_congress_record_id_regex(congresspeople_collection)
+    congressperson_id = re.search(member_ids, speech).group(0) 
     try:
-        opensecrets_id = congressperson_collection.find_one({"congress_record_id" : congressperson_id})['opensecrets_id'] #using opensecrets_id for now, may change
+        opensecrets_id = congresspeople_collection.find_one({"congress_record_id" : congressperson_id})['opensecrets_id'] #using opensecrets_id for now, may change
         return opensecrets_id
     except TypeError as e:
+        # debugging code
         with open('bugged_speech.txt', 'w') as f:
             f.write(speech)
         print(congressperson_id)
         raise e
 
-def extract_speeches(pdf_text, date, chamber, congressperson_collection):
-    # Prepare pdf text to be parsed for speeches
-    # handle newlines
-    pdf_text = re.sub('-|\n', '' ,pdf_text)
-    pdf_text = re.sub('\nf ', '~', pdf_text) #marking the end of a speech
+# extract a list of all speeches from pdf text
+def extract_speeches(pdf_text, date, chamber, congresspeople_collection):
     year = date.year
-
-    #print(re.search('VerDate(.*?)'+year, pdf_text).group()) # no <3
-    pdf_text = re.sub('VerDate(?:.*?)RECORD(?:.*?)' + str(year), '', pdf_text) #test this
-    #VerDate.*year
-    #pdf_text = re.sub('The SPEAKER pro tempore', '~', pdf_text)
-    pdf_text = re.sub('The (?:ACTING )?(?:PRESIDENT|SPEAKER) pro tempore|The PRESIDING OFFICER', '~', pdf_text) #marking the end of a speech
-    #regex variables
-
-    member_ids = get_congress_record_id_regex(congressperson_collection)
-    date = '(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|June?|July?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?) [0-9]{1,2}, [0-9]{4}'
-    states = 'Alabama|Alaska|Arizona|Arkansas|California|Colorado|Connecticut|Delaware|Florida|Georgia|Hawaii|Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|Michigan|Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New Hampshire|New Jersey|New Mexico|New York|North Carolina|North Dakota|Ohio|Oklahoma|Oregon|Pennsylvania|Rhode Island|South Carolina|South Dakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West Virginia|Wisconsin|Wyoming'
-
-    # find all speeches
-    # print("-----")
-    # print(member_ids)
-    # print("-----")
-
+    # handle newlines, mark end of speeches with ~
+    pdf_text = re.sub('-|\n', '' ,pdf_text)
+    pdf_text = re.sub('\nf ', '~', pdf_text) 
+    pdf_text = re.sub('VerDate(?:.*?)RECORD(?:.*?)' + str(year), '', pdf_text) #test this?
+    pdf_text = re.sub('The (?:ACTING )?(?:PRESIDENT|SPEAKER) pro tempore|The PRESIDING OFFICER', '~', pdf_text) 
+    
+    # regex variables
+    member_ids = get_congress_record_id_regex(congresspeople_collection)
     stopper = '(?:(?:Mr|Ms|Mrs)\. (?:' + member_ids + ')\.|~)'
-    #regex = '(?:Mr|Ms|Mrs)\. (?:' + member_ids + ')\. [^~]*' # could def make this better, but works ok!
-    regex = '((?:Mr|Ms|Mrs)\. (?:' + member_ids + ')\. (?:.*?))' + stopper #adding this, lets see if it works
-    #regex = '('+memberID + '(?: of ' + states + ')?\. (?:.*?))(?:~|'+memberID+'\.)'
+    regex = '((?:Mr|Ms|Mrs)\. (?:' + member_ids + ')\. (?:.*?))' + stopper 
     all_speeches = re.findall(regex, pdf_text)
 
-    # should output list of all speeches in the given pdf
-    # for speech in all_speeches:
-    #     print("NEXT SPEECH--------")
-    #     print(speech)
     return all_speeches
-
-# could prob do more filtering like the speech filter
-
-# %% [markdown]
-# Issues to work out:
-# Still getting "CONGRESSIONAL RECORD" and some other stuff as members
-# Idea: make a regex of all member names using the group congress collection -- i think this will be good to have, even if it is maybe a bit less efficient it will avoid these errors 
-# 
-# To do:
-# - connect to server db
-#     - add code to read thru multiple pdfs from the db
-# - integrate congress member collection
-#     - make function to get regex of all members names
-#     - update get cmid function (what actual id are we using?)
-# - try on other pdfs, including ones w/o speeches! **** make changes *here*
-# - better parsing for speeches themselves -- improved getting the speaker "tags", now need to improve getting speeches themselves 
-# - better parsing on initial pdf
-# - figure out what kind of filtering to implement
-# - potentially rewrite whole thing as a script (more typical python)
-# 
-# 
-
