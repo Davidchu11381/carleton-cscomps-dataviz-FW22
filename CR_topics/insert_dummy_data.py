a74db2966a95140b8f7b6514dd90c262fe9163c4
Anna Neiman-Golden <annaneiman-golden@Annas-MacBook-Air-3.local>
2023-01-11 17:34:34 -0600

    inserting dummy data for topics and frequencies

---
+++ CR_topics/insert_dummy_data.py
@@ -0,0 +1,21 @@
+import pymongo
+import random
+
+client = pymongo.MongoClient("mongodb://localhost:27017/") # must be run on olin312-04
+db = client["comps"]
+congresspeople_collection = db["congresspeople"]
+topics_collection = db["statement_topics"]
+
+random_words = ["abortion", "government", "business", "capitalism", "Biden", "president", "guns", "violence", "crime", "rights", "freedom", "immigration", "health", "insurance", "covid", "aid", "vaccines", "women's rights", "policy", "labor", "covid", "Trump", "DACA", "climate change", "environment", "police", "minimum wage", "America", "Russia", "election", "economy"]
+
+for doc in congresspeople_collection.find():
+    num_words = random.randint(2, 6)
+    for i in num_words:
+        frequency = random.random()
+        topic = random_words[random.randint(0,30)]
+        new_doc = {
+            "opensecrets_id" : doc["opensecrets_id"],
+            "topic" : topic,
+            "frequency" : frequency
+        }
+        topics_collection.insert_one(new_doc)
