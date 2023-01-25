c2518716fe8022b21478e9fa914f89cba90c58cd
aoki-sherwoodb <aokib@carleton.edu>
2023-01-24 22:49:29 -0600

    getting data from api to sankey diagram

--- src/pages/HomePage/components/SankeyChart.js
+++ src/pages/HomePage/components/SankeyChart.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react'
 import Chart from 'react-google-charts'
+import { useState, useEffect} from 'react'
+import { data } from 'jquery';
+
 const sankeyData = [
   ['From', 'To', 'Weight'],
-  ['Oil & Gas', 'Politician A', 5],
-  ['Oil & Gas', 'Politician B', 7],
-  ['Oil & Gas', 'Politician C', 6],
-  ['Education', 'Politician A', 2],
-  ['Education', 'Politician B', 9],
-  ['Education', 'Politician C', 4],
+  ['Oil & Gas', 'Politician A', .4],
+  ['Healthcare', 'Politician A', .1],
+  ['Law', 'Politician A', .2],
+  ['Education', 'Politician A', .2],
+  ['Finance', 'Politician A', .1],
+  ['Politician A', 'Abortion', .7],
+  ['Politician A', 'Border',.3],
 ]
+
 class SankeyChart extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+    this.list = [];
+  }
+
+  componentDidMount() {
+    fetch('http://127.0.0.1:5001/N00007360/industry')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ data });
+      });
+  }
+
+  //kind of did it!!
+  formatList() {
+    var sankeyList = [['From', 'To', 'Weight']]
+    var industries = this.state.data.industry 
+    for (let ind in industries){
+      console.log(industries[ind].industry_name)
+      sankeyList.push([industries[ind].industry_name, 'Nancy Pelosi', parseInt(industries[ind].total)])
+
+    }
+    console.log(sankeyList)
+    return(sankeyList)
+  }
+
   render() {
+    let sankeyData = this.formatList()
     return (
       <div className="container mt-5">
         <h2>Sankey Diagram</h2>
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Container, Row, Col, Button} from 'react-bootstrap';
 // import YoutubeEmbed from './components/YoutubeEmbed';
-// import SankeyChart from './components/SankeyChart';
+import SankeyChart from './components/SankeyChart';
 import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
@@ -80,7 +80,7 @@ function HomePage() {
                     </Col>
 
                 </Row>
-                {/* <SankeyChart /> */}
+                <SankeyChart />
             </div>
         </Container>
     );
