0efe18e39c5d3fd99e6e005e2ed5044162a292cc
Kevin Chen <usa.kevin@gmail.com>
2023-01-30 19:07:40 -0600

    Delete test.py

---  test.py
+++
@@ -1,5 +0,0 @@
-import requests
-import json
-
-# requests.post("http://127.0.0.1:5001/N00007360/topics", data=json.dumps({"name": ["foo", "poo", "koo"]}))
-requests.post("http://127.0.0.1:5001/N00007360/topics")
