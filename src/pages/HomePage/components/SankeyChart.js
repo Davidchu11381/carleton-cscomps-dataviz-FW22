d7543ef1e229f5f2269adebcbc6bee46f3ac820b
aoki-sherwoodb <aokib@carleton.edu>
2023-02-06 22:52:48 -0600

    added group and multiple cid functionality to sankey chart, fixed cors errors in api

--- src/pages/HomePage/components/SankeyChart.js
+++ src/pages/HomePage/components/SankeyChart.js
@@ -5,28 +5,50 @@ class SankeyChart extends Component {
 
   constructor(props) {
     super(props);
-    this.cid = props.cid; //pass in cid as props in html -- can turn into a list of cids
+    this.cid_list = props.cid_list; //pass in cid_list as props in html
+    this.group = props.group
     this.state = {
-      data : []
+      ind_data : []
     };
+    //this.data = [];
     this.fetchData();
   }
 
   fetchData() {
-    // fetch('http://137.22.4.60:5001/'+ this.cid +'/industry') 
-    fetch('http://127.0.0.1:5000/'+ this.cid +'/industry') 
+    console.log("group:", this.group)
+    if (this.group === undefined) {
+      console.log("in if")
+      //fetch('http://127.0.0.1:5000/'+ this.cid +'/industry') 
+      fetch('http://137.22.4.60:5001/'+ this.cid_list +'/industry') 
+        .then(response => response.json())
+        .then(ind_data => {
+          console.log("the data:", ind_data);
+          this.setState({ ind_data });
+          //this.setData(ind_data)
+        });
+    } else {
+      console.log("in else")
+      fetch('http://137.22.4.60:5001/'+ this.group +'/aggregate') 
       .then(response => response.json())
-      .then(data => {
-        console.log("the data:", data);
-        this.setState({ data });
+      .then(ind_data => {
+        console.log("the data:", ind_data);
+        console.log("industry specifically: ", ind_data.industry)
+        this.setState({ ind_data });
       });
+    }
   }
 
   formatList() {
+    // determine what to title the middle column of the sankey
+    if (this.group === undefined) {
+      var mid_col = this.cid_list //gonna have to change, just a cid right now not a name
+    } else {
+      var mid_col = this.group
+    }
     var sankeyList = [['From', 'To', 'Weight']]
-    var industries = this.state.data.industry 
+    var industries = this.state.ind_data.industry 
     for (let ind in industries){
-      sankeyList.push([industries[ind].industry_name, this.state.data.cand_name, parseInt(industries[ind].total)]);
+      sankeyList.push([industries[ind].industry_name, mid_col, parseInt(industries[ind].total)]);
     }
     console.log(sankeyList)
     return(sankeyList)
